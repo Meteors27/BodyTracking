49f23557bcde558c4bda2ab9294d9cc36689b98b
meteors27 <xia.yucheng@outlook.com>
2024-06-20 01:56:48 +0800

    fix: display old skeleton when resume

--- BodyTracking/ARViewContainer.swift
+++ BodyTracking/ARViewContainer.swift
@@ -34,6 +34,8 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
     func resumeARView() {
+        bodySkeletonAnchor.children.removeAll()
+        bodySkeleton = nil
         arView.session.run(ARBodyTrackingConfiguration())
     }
     
