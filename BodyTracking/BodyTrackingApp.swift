6599e685b126522ce6fe2dfd2b273d365471f61e
meteors27 <xia.yucheng@outlook.com>
2024-06-19 20:08:32 +0800

    Initial Commit

---
+++ BodyTracking/BodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingApp.swift
+//  BodyTracking
+//
+//  Created by william on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
