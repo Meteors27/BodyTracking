a453bb5185ec8a4afb5d885c7987051595cd7b7c
meteors27 <xia.yucheng@outlook.com>
2024-06-19 21:38:11 +0800

    feat: add data structure

---
+++ BodyTracking/BodySkeleton.swift
@@ -0,0 +1,8 @@
+//
+//  BodySkeleton.swift
+//  BodyTracking
+//
+//  Created by william on 2024/6/19.
+//
+
+import Foundation
--- BodyTracking/SkeletonJoint.swift
+++ BodyTracking/SkeletonJoint.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+}
